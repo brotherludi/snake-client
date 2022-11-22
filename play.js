0efe0184409a550b563cb95ab0586863bef19ccf
Di Lu <brotherludi@hotmail.com>
2022-11-21 18:44:10 -0700

    Stdin Setup

--- play.js
+++ play.js
@@ -10,11 +10,10 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: BOB');
-  //   setTimeout(() => {
-  //     conn.write("Move: up")
-  //   },2000)
-  // });
-
+    // setTimeout(() => {
+    //   conn.write("Move: up")
+    // },1000)
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (data) => {
@@ -22,11 +21,36 @@ const connect = function () {
   });
   return conn;
 };
+// setup interface to handle user input from stdin
+const handleUserInput = function (key) {
+  console.log("key", key)
+  if (key == '\u001B\u005B\u0041') {
+    process.stdout.write('up');
+  }
+  if (key == '\u001B\u005B\u0043') {
+    process.stdout.write('right');
+  }
+  if (key == '\u001B\u005B\u0042') {
+    process.stdout.write('down');
+  }
+  if (key == '\u001B\u005B\u0044') {
+    process.stdout.write('left');
+  }
+  if (key == '\u0003') { process.exit(); }
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
+const input = setupInput()
+input.on("data", handleUserInput);
 console.log("Connecting ...");
 connect();
 
 module.exports = {
-  connect 
+  connect
 };
-
