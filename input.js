9dc426df2e894ac82d949aed6d10b1e3e3113e3f
Di Lu <brotherludi@hotmail.com>
2022-11-23 18:27:06 -0700

    Sending Messages

--- input.js
+++ input.js
@@ -1,21 +1,6 @@
-// setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  console.log("key", key)
-  if (key == '\u001B\u005B\u0041') {
-    process.stdout.write('up');
-  }
-  if (key == '\u001B\u005B\u0043') {
-    process.stdout.write('right');
-  }
-  if (key == '\u001B\u005B\u0042') {
-    process.stdout.write('down');
-  }
-  if (key == '\u001B\u005B\u0044') {
-    process.stdout.write('left');
-  }
-  if (key == '\u0003') { process.exit(); }
-};
-const setupInput = function () {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -23,8 +8,27 @@ const setupInput = function () {
   stdin.resume();
   return stdin;
 };
-
-module.exports = {
-  setupInput
+// setup interface to handle user input from stdin
+const handleUserInput = function (key) {
+  console.log("key", key)
+  if (key == '\u001B\u005B\u0041') {
+    connection.write('Move: up');
+  }
+  if (key == '\u001B\u005B\u0043') {
+    connection.write('Move: right');
+  }
+  if (key == '\u001B\u005B\u0042') {
+    connection.write('Move: down');
+  }
+  if (key == '\u001B\u005B\u0044') {
+    connection.write('Move: left');
+  }
+  if (key == 'm') {
+    connection.write("Say: HELLO")
+  }
+  if (key == '\u0003') { process.exit(); }
 };
+
+
+module.exports = { setupInput };
 
